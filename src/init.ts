b1b361fe29bb5973b5a78fb689272672f101e54e
Rafael Bardini <rafael@rbardini.com>
2023-01-17 02:35:45 +0100

    fix(init): create `require` to import JSON

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,8 @@
 import { writeFile } from 'node:fs/promises'
+import { createRequire } from 'node:module'
+
+// ESM can't import JSON yet, fallback to `require`
+const require = createRequire(import.meta.url)
 
 export const init = (filename: string) => {
   const resume = require('resume-schema/sample.resume.json')
