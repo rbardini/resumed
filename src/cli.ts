002ea393debac05b0bb1536320feebfda29c48c5
Tom Whitwell <tom@whi.tw>
2023-06-30 17:09:00 +0100

    Add `watch` and `serve` commands
    - `watch` will watch for changes to the json file and rebuild the resume
    - `serve` will serve the resume on a local server and live-reload when
      changes are made to the json file

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,12 @@ import { red, yellow } from 'yoctocolors'
 import sade from 'sade'
 import { init, render, validate } from './index.js'
 
+import express from 'express'
+import livereload from 'livereload'
+import connectLiveReload from 'connect-livereload'
+
+import chokidar from 'chokidar'
+
 // Trick Rollup into not bundling package.json
 const pkgPath = '../package.json'
 const pkg = JSON.parse(
@@ -95,3 +101,108 @@ cli
       process.exitCode = 1
     }
   })
+
+cli
+  .command('serve [filename]', 'Serve resume')
+  .option('-t, --theme', 'Theme to use')
+  .option('-p, --port', 'Port to use', 3000)
+  .action(
+    async (
+      filename: string = 'resume.json',
+      port: number = 3000,
+      theme: string,
+    ) => {
+      const app = express()
+      app.use(connectLiveReload())
+
+      const liveReloadServer = livereload.createServer({
+        extraExts: ['json'],
+        debug: true,
+      })
+      liveReloadServer.watch(filename)
+
+      app.get('/', async (_, res) => {
+        const resume = JSON.parse(await readFile(filename, 'utf-8'))
+        const themeName = theme ?? resume?.meta?.theme
+        if (!themeName) {
+          console.error(
+            `No theme to use. Please specify one via the ${yellow(
+              '--theme',
+            )} option or the ${yellow('.meta.theme')} field of your resume.`,
+          )
+          const msg = `No theme to use. Please restart the server with the '--theme' option or set the '.meta.theme' field of your resume.`
+          res.send(
+            `<html><head><title>Error</title><body><pre>${msg}</pre></body></html>`,
+          )
+          return
+        }
+
+        let themeModule
+        try {
+          themeModule = await import(themeName)
+        } catch {
+          console.error(
+            `Could not load theme ${yellow(themeName)}. Is it installed?`,
+          )
+
+          const msg = `Could not load theme ${themeName}. Is it installed?`
+          res.send(
+            `<html><head><title>Error</title><body><pre>${msg}</pre></body></html>`,
+          )
+          return
+        }
+
+        res.send(await render(resume, themeModule))
+      })
+
+      app.listen(port, () => {
+        console.log(`Resume available at http://0.0.0.0:${port}/`)
+      })
+    },
+  )
+
+cli
+  .command('watch [filename]', 'Watch for changes and render resume')
+  .option('-o, --output', 'Output filename', 'resume.html')
+  .option('-t, --theme', 'Theme to use')
+  .action(
+    async (
+      filename: string = 'resume.json',
+      { output, theme }: RenderOptions,
+    ) => {
+      chokidar
+        .watch(filename)
+        .on('ready', async () => {
+          console.log(`Watching for changes to ${yellow(filename)}...`)
+        })
+        .on('change', async () => {
+          const resume = JSON.parse(await readFile(filename, 'utf-8'))
+          const themeName = theme ?? resume?.meta?.theme
+          if (!themeName) {
+            console.error(
+              `No theme to use. Please specify one via the ${yellow(
+                '--theme',
+              )} option or the ${yellow('.meta.theme')} field of your resume.`,
+            )
+            return
+          }
+
+          let themeModule
+          try {
+            themeModule = await import(themeName)
+          } catch {
+            console.error(
+              `Could not load theme ${yellow(themeName)}. Is it installed?`,
+            )
+            return
+          }
+
+          const rendered = await render(resume, themeModule)
+          await writeFile(output, rendered)
+
+          console.log(
+            `${yellow(filename)} updated, re-rendered to ${yellow(output)}.`,
+          )
+        })
+    },
+  )
